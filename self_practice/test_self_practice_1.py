401ec517da4559f017b805cf85259bcbab8cde25
YauheniWind <78211102+YauheniWind@users.noreply.github.com>
2023-04-20 21:18:31 +0200

    upgrade code with fixture and conftest

--- self_practice/test_self_practice_1.py
+++ self_practice/test_self_practice_1.py
@@ -1,17 +1,10 @@
-import time
-
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-def test_show_product():
-    driver = webdriver.Chrome(
-        "/Users/evgenijgravdin/Desktop/tms_aqa_python/chromedriver"
-    )
-    driver.fullscreen_window()
-    driver.get("https://www.istores.sk")
-    time.sleep(1)
-    show_product = driver.find_element(
+def test_show_product(browser):
+    browser.fullscreen_window()
+    browser.get("https://www.istores.sk")
+    show_product = browser.find_element(
         By.XPATH, '//*[@id="wnCarousel24"]/div[2]/div[2]/div/div[1]'
     )
     show_product.click()
@@ -21,81 +14,58 @@ def test_show_product():
         assert h1_xpath == "iPhone 11 64 GB čierny"
     else:
         assert h1_xpath != "iPhone 11 64 GB čierny"
-    driver.quit()
 
 
-def test_hamburger_menu():
-    driver = webdriver.Chrome(
-        "/Users/evgenijgravdin/Desktop/tms_aqa_python/chromedriver"
-    )
-    driver.get("https://www.istores.sk")
-    time.sleep(2)
+def test_hamburger_menu(browser):
+    browser.get("https://www.istores.sk")
 
-    driver.set_window_size(760, 600)
+    browser.set_window_size(760, 600)
     hamburger_menu = '//*[@id="tree-icon"]'
     element_iphone = '//*[@id="sptnav"]/ul/li[4]/a'
     element_iphone_pro_max = '//*[@id="subcatscont"]/div[2]/ul/li[2]/a'
-    hamburger_locator = driver.find_element(By.XPATH, hamburger_menu)
-    iphone_locator = driver.find_element(By.XPATH, element_iphone)
+    hamburger_locator = browser.find_element(By.XPATH, hamburger_menu)
+    iphone_locator = browser.find_element(By.XPATH, element_iphone)
 
     hamburger_locator.click()
     iphone_locator.click()
-    time.sleep(2)
 
-    iphone_locator_pro_max = driver.find_element(By.XPATH, element_iphone_pro_max)
+    iphone_locator_pro_max = browser.find_element(By.XPATH, element_iphone_pro_max)
     iphone_locator_pro_max.click()
 
     name = '//*[@id="contentcont"]/div[2]/div/div[1]/div[1]/div[2]/h1'
-    name_locator = driver.find_element(By.XPATH, name).text
+    name_locator = browser.find_element(By.XPATH, name).text
 
     assert name_locator == "iPhone 14 Pro Max 128 GB kozmicky čierny"
-    driver.quit()
 
 
-def test_search():
-    driver = webdriver.Chrome(
-        "/Users/evgenijgravdin/Desktop/tms_aqa_python/chromedriver"
-    )
-    driver.get("https://www.istores.sk")
-    time.sleep(2)
+def test_search(browser):
+    browser.get("https://www.istores.sk")
 
-    driver.fullscreen_window()
     search_input = '//*[@id="whisperinput"]'
-    search_locator = driver.find_element(By.XPATH, search_input)
-    time.sleep(2)
+    search_locator = browser.find_element(By.XPATH, search_input)
+
     search_locator.send_keys("iphone")
-    search_locator.send_keys(webdriver.Keys.ENTER)
-    time.sleep(2)
+    search_locator.send_keys(browser.Keys.ENTER)
+
     searched_element = '//*[@id="contentcont"]/div[3]/div[1]/p'
-    searched_locator = driver.find_element(By.XPATH, searched_element).text
+    searched_locator = browser.find_element(By.XPATH, searched_element).text
 
     assert searched_locator == "Hľadanie: iphone"
-    driver.quit()
 
 
-def test_log_in():
-    driver = webdriver.Chrome(
-        "/Users/evgenijgravdin/Desktop/tms_aqa_python/chromedriver"
-    )
-    driver.get("https://www.saucedemo.com/")
-    time.sleep(1)
+def test_log_in(browser):
+    browser.get("https://www.saucedemo.com/")
 
-    driver.fullscreen_window()
     user_name = '//*[@id="user-name"]'
     password = '//*[@id="password"]'
-    user_name_locator = driver.find_element(By.XPATH, user_name)
-    user_password_locator = driver.find_element(By.XPATH, password)
+    user_name_locator = browser.find_element(By.XPATH, user_name)
+    user_password_locator = browser.find_element(By.XPATH, password)
 
-    time.sleep(1)
     user_name_locator.send_keys("standard_user")
     user_password_locator.send_keys("secret_sauce")
-    time.sleep(1)
 
     button = '//*[@id="login-button"]'
-    pressed_button = driver.find_element(By.XPATH, button)
+    pressed_button = browser.find_element(By.XPATH, button)
     pressed_button.click()
 
-    assert driver.current_url == "https://www.saucedemo.com/inventory.html"
-    time.sleep(2)
-
-    driver.quit()
+    assert browser.current_url == "https://www.saucedemo.com/inventory.html"
--- self_practice/test_tools_qa.py
+++ self_practice/test_tools_qa.py
@@ -1,13 +1,9 @@
-import time
-
-from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 
 
-def test_text_box():
-    browser = webdriver.Chrome()
+def test_text_box(browser):
     browser.get("https://demoqa.com/text-box")
-    time.sleep(1)
 
     full_name_xpath = '//*[@id="userName"]'
     email_xpath = '//*[@id="userEmail"]'
@@ -25,13 +21,11 @@ def test_text_box():
     email_locator.send_keys("adamnor938@gmail.com")
     current_address_locator.send_keys("Lomnicka 124")
     permanent_address_locator.send_keys("Novozamocaka 22")
-    time.sleep(2)
     submit_locator.click()
     browser.quit()
 
 
-def test_checkbox():
-    browser = webdriver.Chrome()
+def test_checkbox(browser):
     browser.get("https://demoqa.com/checkbox")
 
     menu_xpath = '//*[@id="tree-node"]/ol/li/span/button'
@@ -39,48 +33,41 @@ def test_checkbox():
 
     home_checkbox_xpath = '//*[@id="tree-node"]/ol/li/span/label/span[1]'
     home_checkbox_locator = browser.find_element(By.XPATH, home_checkbox_xpath)
-    time.sleep(2)
     home_checkbox_locator.click()  # active checkbox
     menu_locator.click()  # open manu
-    time.sleep(2)
 
     hidden_text_xpath = '//*[@id="result"]'
     hidden_text_locator = browser.find_element(By.XPATH, hidden_text_xpath)
 
     if hidden_text_locator:
         print("Passed")
-    time.sleep(2)
 
 
-def test_radio_button():
-    browser = webdriver.Chrome()
+def test_radio_button(browser):
     browser.get("https://demoqa.com/radio-button")
 
     radio_button_xpath = '//label[@for="yesRadio"]'
     radio_button_locator = browser.find_element(By.XPATH, radio_button_xpath)
     radio_button_text = radio_button_locator.text
     radio_button_locator.click()
-    time.sleep(2)
+
     span_text_xpath = '//*[@id="app"]/div/div/div[2]/div[2]/div[2]/p/span'
     span_text_locator_text = browser.find_element(By.XPATH, span_text_xpath).text
     assert radio_button_text == span_text_locator_text
 
 
-def test_buttons():
-    browser = webdriver.Chrome()
+def test_buttons(browser):
     browser.get("https://demoqa.com/buttons")
-    action = webdriver.ActionChains(browser)
+    action = ActionChains(browser)
 
     double_click_button_xpath = '//button[@id="doubleClickBtn"]'
     double_click_button_locator = browser.find_element(
         By.XPATH, double_click_button_xpath
     )
     action.double_click(double_click_button_locator).perform()
-    time.sleep(5)
 
     right_click_button_xpath = '//button[@id="rightClickBtn"]'
     right_click_button_locator = browser.find_element(
         By.XPATH, right_click_button_xpath
     )
     action.context_click(right_click_button_locator).perform()
-    time.sleep(2)
